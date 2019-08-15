c38d622415deaccf83ad22556903a67d57d75719
poncoe <poncoe4mac@gmail.com>
2019-08-15 09:05:13 +0700

    added kucing

---
+++ app/src/main/java/id/poncoe/latihanmasterandroidponcoe/Kotlin/recyclerview/Kucing.kt
@@ -0,0 +1,6 @@
+package id.poncoe.latihanmasterandroidponcoe.Kotlin.recyclerview
+
+data class Kucing(
+        var name: String = "",
+        var from: String = "",
+        var photo: String = "")
