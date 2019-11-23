9b26cfc0145259df8fff21a96071c919cb2f6a7a
poncoe <poncoe4mac@gmail.com>
2019-11-24 00:44:16 +0700

    Belajar Unit Testing Java

---
+++ app/src/main/java/id/poncoe/latihanmasterandroidponcoe/java/unittesting/MainViewModel.java
@@ -0,0 +1,20 @@
+package id.poncoe.latihanmasterandroidponcoe.java.unittesting;
+
+class MainViewModel {
+    private final CuboidModel cuboidModel;
+    MainViewModel(CuboidModel cuboidModel) {
+        this.cuboidModel = cuboidModel;
+    }
+    void save(double l, double w, double h) {
+        cuboidModel.save(l, w, h);
+    }
+    double getCircumference() {
+        return cuboidModel.getCircumference();
+    }
+    double getSurfaceArea() {
+        return cuboidModel.getSurfaceArea();
+    }
+    double getVolume() {
+        return cuboidModel.getVolume();
+    }
+}
