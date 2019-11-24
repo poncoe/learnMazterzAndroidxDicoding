c3229b1697844d5a4ffee26f4a9632ae8aa9eccd
poncoe <poncoe4mac@gmail.com>
2019-11-25 00:46:25 +0700

    Belajar Sqilite Kotlin

---
+++ app/src/main/java/id/poncoe/latihanmasterandroidponcoe/kotlin/storage/sqlite/CustomOnItemClickListener.kt
@@ -0,0 +1,13 @@
+package id.poncoe.latihanmasterandroidponcoe.kotlin.storage.sqlite
+
+import android.view.View
+
+class CustomOnItemClickListener(private val position: Int, private val onItemClickCallback: OnItemClickCallback) : View.OnClickListener {
+    override fun onClick(view: View) {
+        onItemClickCallback.onItemClicked(view, position)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(view: View, position: Int)
+    }
+}
---
+++ app/src/main/java/id/poncoe/latihanmasterandroidponcoe/kotlin/storage/sqlite/helper/MappingHelper.kt
@@ -0,0 +1,26 @@
+package id.poncoe.latihanmasterandroidponcoe.kotlin.storage.sqlite.helper
+
+import android.database.Cursor
+import id.poncoe.latihanmasterandroidponcoe.kotlin.storage.sqlite.db.DatabaseContract
+
+import id.poncoe.latihanmasterandroidponcoe.kotlin.storage.sqlite.entity.Note
+
+import java.util.ArrayList
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor.moveToFirst()
+        while (notesCursor.moveToNext()) {
+            val id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(
+                DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+            notesList.add(Note(id, title, description, date))
+        }
+
+        return notesList
+    }
+}
