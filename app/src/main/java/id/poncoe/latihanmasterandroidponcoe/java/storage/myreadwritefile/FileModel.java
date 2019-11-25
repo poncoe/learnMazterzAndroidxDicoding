a9f02ecab09e61f0e75c607423ffee15cda00edc
poncoe <poncoe4mac@gmail.com>
2019-11-26 00:49:37 +0700

    Belajar My Read Write File Java

---
+++ app/src/main/java/id/poncoe/latihanmasterandroidponcoe/java/storage/myreadwritefile/FileModel.java
@@ -0,0 +1,22 @@
+package id.poncoe.latihanmasterandroidponcoe.java.storage.myreadwritefile;
+
+public class FileModel {
+    private String filename;
+    private String data;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
