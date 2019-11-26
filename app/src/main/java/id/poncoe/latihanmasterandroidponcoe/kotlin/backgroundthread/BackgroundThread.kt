f43262b57a98bad46e794206a543dcb50d7a9190
poncoe <poncoe4mac@gmail.com>
2019-11-27 00:51:26 +0700

    Background Thread Kotlin

---
+++ app/src/main/java/id/poncoe/latihanmasterandroidponcoe/kotlin/backgroundthread/BackgroundThread.kt
@@ -0,0 +1,54 @@
+package id.poncoe.latihanmasterandroidponcoe.kotlin.backgroundthread
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import id.poncoe.latihanmasterandroidponcoe.R
+import kotlinx.android.synthetic.main.activity_background_thread.*
+import kotlinx.coroutines.*
+
+class BackgroundThread : AppCompatActivity() {
+
+    companion object {
+        private const val INPUT_STRING = "Halo Ini Demo AsyncTask!!"
+        private const val LOG_ASYNC = "DemoAsync"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_status.setText(R.string.status_pre)
+        tv_desc.text =
+            INPUT_STRING
+
+        GlobalScope.launch(Dispatchers.Default) {
+            //background thread
+            val input =
+                INPUT_STRING
+
+            var output: String? = null
+
+            Log.d(LOG_ASYNC, "status : doInBackground")
+            try {
+                // Input stringnya ditambahkan dengan string ' Selamat Belajar!!"
+                output = "$input Selamat Belajar!!"
+
+                /*
+                Sleep thread digunakan untuk simulasi bahwa ada proses yang sedang berjalan selama 2 detik
+                2000 miliseconds = 2 detik
+                */
+                delay(2000)
+
+                //pindah ke Main thread untuk update UI
+                withContext(Dispatchers.Main) {
+                    tv_status.setText(R.string.status_post)
+                    tv_desc.text = output
+                }
+
+            } catch (e: Exception) {
+                Log.d(LOG_ASYNC, e.message.toString())
+            }
+        }
+    }
+}
